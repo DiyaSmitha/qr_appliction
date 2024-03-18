a46b405ace818df7a0fc17a34eb78e97df6e684c
DiyaSmitha <diyasmitha04@gmail.com>
2024-03-18 15:56:34 +0530

    gojoo00

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:qrappliction/qr.dart';
 import 'package:qrappliction/registor.dart';
-
+import 'package:http/http.dart'as http;
 class index extends StatefulWidget {
   const index({Key? key}) : super(key: key);
 
@@ -10,6 +12,42 @@ class index extends StatefulWidget {
 }
 
 class _indexState extends State<index> {
+  TextEditingController rollno =TextEditingController();
+  TextEditingController password =TextEditingController();
+  void log() async{
+    print(rollno.text);
+    print(password.text);
+    Uri uri=Uri.parse('https://scnner-web.onrender.com/api/login');
+    var response = await http.post(uri,
+        headers:<String,String>{
+        'Content-Type':'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'rollno':rollno.text,
+          'password':password.text
+        }));
+    print(response.statusCode);
+    print(response.body);
+    var data =jsonDecode(response.body);
+    print(data["message"]);
+    if(response.statusCode==200){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => index() ,));
+    }
+    else{ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: data["message"]));}
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +69,7 @@ class _indexState extends State<index> {
               height: 10,
             ),
             TextField(
+              controller: rollno ,
               decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(40),),
@@ -45,6 +84,7 @@ class _indexState extends State<index> {
               height: 25,
             ),
             TextField(
+              controller: password,
               decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(40),),
@@ -66,7 +106,7 @@ class _indexState extends State<index> {
               onPressed: () {
 
 
-                Navigator.push(context, MaterialPageRoute(builder: (context) => qrpage() ,));
+               log();
 
               },
               child: Text(
